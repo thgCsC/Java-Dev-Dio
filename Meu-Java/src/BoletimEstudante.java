c3b5ca8ee9a723551b54f77a56dc5e8a093b2156
thgCSC <thyagoshadows@gmail.com>
2024-01-16 22:10:23 -0300

    Implementar esse algortimo ainda mais... ESTUDO ACADEMICO....

---
+++ Meu-Java/src/BoletimEstudante.java
@@ -0,0 +1,12 @@
+public class BoletimEstudante {
+    public static void main(String[] args) {
+        int mediaFinal = 5;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("O ALUNO ESTÁ DE RECUPERAÇÃO");
+        else
+            System.out.println("ALUNO APROVADO");
+    }
+}
+// IMPLEMENTAR ESSE ALGORITMO AINDA MAIS =D
