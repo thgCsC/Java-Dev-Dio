8a231896c71671671794d5f71af28d7b1cf1bc8d
thgCSC <thyagoshadows@gmail.com>
2024-01-17 21:48:52 -0300

    Implementado o input... vamos implementar as formatações.

--- Meu-Java/src/estudo/thg/primeirasemana/BoletimEstudante.java
+++ Meu-Java/src/estudo/thg/primeirasemana/BoletimEstudante.java
@@ -1,15 +1,26 @@
 package estudo.thg.primeirasemana;
 
+import java.util.Scanner;
+
 public class BoletimEstudante {
     public static void main(String[] args) {
-        int mediaFinal = 5;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a primeira nota do aluno:");
+        float nota1 = sc.nextFloat();
+
+        System.out.println("Digite a segunda nota do aluno:");
+        float nota2 = sc.nextFloat();
+
+        float mediaFinal = (nota1 + nota2) / 2;
         if (mediaFinal < 6)
             System.out.println("REPROVADO");
         else if (mediaFinal == 6)
             System.out.println("O ALUNO ESTÁ DE RECUPERAÇÃO");
         else
-            System.out.println("ALUNO APROVADO");
+            System.out.print("Nota: " + mediaFinal + "\n" +
+                    "ALUNO APROVADO.");
     }
 }
 // IMPLEMENTAR ESSE ALGORITMO AINDA MAIS =D
-// adicionar um imput
+// adicionar um imput - Implementado √
