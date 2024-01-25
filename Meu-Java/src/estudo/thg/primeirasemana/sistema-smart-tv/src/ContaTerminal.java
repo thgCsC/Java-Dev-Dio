52a32712e78dd7e1d8387b63c15098a9ffd6666e
thgCSC <thyagoshadows@gmail.com>
2024-01-24 22:17:26 -0300

    Desafio concluído

--- Meu-Java/src/estudo/thg/primeirasemana/sistema-smart-tv/src/ContaTerminal.java
+++ Meu-Java/src/estudo/thg/primeirasemana/sistema-smart-tv/src/ContaTerminal.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            int Numero;
-            String Agencia, nomeCliente;
-            double Saldo;
+            // int Numero;
+            // String Agencia;
+            // String nomeCliente;
+            // double Saldo;
 
             System.out.println("Por favor, digite o número da sua Conta: ");
-            Numero = sc.nextInt();
+            int Numero = sc.nextInt();
 
             System.out.println("Por favor, digite o número da Agência: ");
-            Agencia = sc.nextLine();
-
-            System.out.println("Por favor, digite o seu nome completo: ");
-            nomeCliente = sc.nextLine();
+            String Agencia = sc.nextLine();
 
             System.out.println("Por favor, digite o valor que deseja depositar: ");
-            Saldo = sc.nextDouble();
+            double Saldo = sc.nextDouble();
+
+            System.out.println("Por favor, digite o seu nome completo: ");
+            String nomeCliente = sc.nextLine();
 
             System.out.printf("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
                     + Agencia + ", conta" + Numero + "e seu saldo ", Saldo, "já está disponível para saque.");
