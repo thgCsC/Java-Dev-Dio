d1905d14adb1355ff45d3e79dc37bf90aeb05b90
thgCSC <thyagoshadows@gmail.com>
2024-01-25 23:20:51 -0300

    implementaremos uma interação com o usuário...

--- Meu-Java/src/estudo/thg/primeirasemana/controle-fluxo/controle-fluxo/src/CaixaEletronico.java
+++ Meu-Java/src/estudo/thg/primeirasemana/controle-fluxo/controle-fluxo/src/CaixaEletronico.java
@@ -1,7 +1,7 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 26.0;
+        double valorSolicitado = 25.0;
 
         if (valorSolicitado <= saldo) {
             saldo = saldo - valorSolicitado;
