1c7ce65e23bb46ef123f1c72a61679b9a70e58eb
thgCSC <thyagoshadows@gmail.com>
2024-01-28 18:19:10 -0300

    Readme commitado

---
+++ Meu-Java/src/estudo/thg/primeirasemana/controle-fluxo/controle-fluxo/src/Execercicio.java
@@ -0,0 +1,17 @@
+public class Execercicio {
+    public static void main(String[] args) throws Exception {
+        double alturaJoao = 1.46;
+
+        double alturaMarcos = 1.37;
+
+        if (alturaMarcos > alturaJoao)
+
+            System.out.println("João é mais alto que Marcos");
+
+        else
+
+            System.out.println("Marcos é mais baixo que João");
+
+    }
+
+}
